128f743fc983e6732ee4d91726bbc5df39b28344
rittz009 <riddhir158@gmail.com>
2023-12-13 17:28:56 -0600

    added new files for project 2

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,4 +1,5 @@
 import project1 from '../assets/Project1.jpeg';
+import project2 from '../assets/Project2.Png'
 
 const ProjectCardData = [
     {
@@ -7,6 +8,13 @@ const ProjectCardData = [
         text: "A dynamic and modern portfolio website created using React JS: A front-end JavaScript library showcasing myself, projects, and achievements with a smooth and interactive user experience.",
         view: "https://littleflowerbud.netlify.app",
         source: "https://github.com/rittz1998/react-portfolio"
+    },
+    {
+        imagesource: project2,
+        title: "PocketPal (Student Expense Tracker )",
+        text: "PocketPal uses MVC architecture and incorporates the use of Observer and Factory Design Patterns. It is an application that supports expense tracking as well as advising from Finance advisors.",
+        view: "https://peaceful-waters-10658-52f8f7382eec.herokuapp.com/",
+        source: "https://github.com/pooja-p-patel/CS476_Project_PocketPal"
     }
 ];
 
